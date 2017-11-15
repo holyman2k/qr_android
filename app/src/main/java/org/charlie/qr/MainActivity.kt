3736bcb7e24012bf25c1eeccb1f20079da7cc371
Charlie Wu <c.wu@webfm.net>
2017-11-15 11:28:51 +1100

    request permission

--- app/src/main/java/org/charlie/qr/MainActivity.kt
+++ app/src/main/java/org/charlie/qr/MainActivity.kt
@@ -1,17 +1,31 @@
 package org.charlie.qr
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import com.google.zxing.Result
 import me.dm7.barcodescanner.zxing.ZXingScannerView
 
 class MainActivity : AppCompatActivity(), ZXingScannerView.ResultHandler {
+
+    companion object {
+        const val REQUEST_CODE_PERMISSION = 1000
+    }
 
     private val scannerView: ZXingScannerView by lazy { ZXingScannerView(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(scannerView)
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), REQUEST_CODE_PERMISSION)
+        }
 
     }
 
@@ -29,6 +43,16 @@ class MainActivity : AppCompatActivity(), ZXingScannerView.ResultHandler {
     override fun handleResult(result: Result?) {
         result?.let { qrResult ->
             ResultActivity.start(this, qrResult.text)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_CODE_PERMISSION) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Camera Permission is Required for QR Scanning", Toast.LENGTH_SHORT).show()
+            } else {
+                scannerView.startCamera()
+            }
         }
     }
 }
