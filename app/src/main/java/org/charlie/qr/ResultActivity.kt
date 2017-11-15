6bf4d8b506a9e5862f49112b824ea5227311893c
Charlie Wu <c.wu@webfm.net>
2017-11-15 11:20:15 +1100

    code clean up

--- app/src/main/java/org/charlie/qr/ResultActivity.kt
+++ app/src/main/java/org/charlie/qr/ResultActivity.kt
@@ -6,8 +6,6 @@ import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_result.*
 
